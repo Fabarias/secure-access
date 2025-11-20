0694fceaf7d92510544482b3fe09b6b3f62165e9
Fabricio <fabricioayaranga@gmail.com>
2025-11-20 02:56:10 -0500

    creacion de view con estilos CSS y controller de listadoPoliciaDelincuentes

--- secure-access-frontend/src/main/java/org/secureaccess/app/secureaccessfrontend/controllers/menuControllers/listadoDelincuentesControllers/ListadoPoliciaDelincuentesController.java
+++ secure-access-frontend/src/main/java/org/secureaccess/app/secureaccessfrontend/controllers/menuControllers/listadoDelincuentesControllers/ListadoPoliciaDelincuentesController.java
@@ -1,43 +1,55 @@
 package org.secureaccess.app.secureaccessfrontend.controllers.menuControllers.listadoDelincuentesControllers;
 
-
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.secureaccess.app.secureaccessbackend.modelos.Usuario;
+import org.secureaccess.app.secureaccessfrontend.controllers.menuControllers.OpcionesDelMenuPoliciaController;
 import org.secureaccess.app.secureaccessfrontend.viewModels.DelincuentePoliciaView;
+
+import java.io.IOException;
 
 public class ListadoPoliciaDelincuentesController {
 
-    @FXML
-    private TextField filtroNombre;
+    @FXML private TextField filtroNombre;
+    @FXML private TextField filtroApellidos;
+    @FXML private ComboBox<String> comboDepartamentos;
+    @FXML private TableView<DelincuentePoliciaView> tablaDelincuentes;
+    @FXML private TableColumn<DelincuentePoliciaView, String> columnaNombre;
+    @FXML private TableColumn<DelincuentePoliciaView, String> columnaApellidos;
+    @FXML private TableColumn<DelincuentePoliciaView, String> columnnaDelito;
+    @FXML private TableColumn<DelincuentePoliciaView, Double> columnaRecompensa;
+    @FXML private ObservableList<DelincuentePoliciaView> listaObservableDelincuente;
+
+    private Usuario usuarioActual;
+
+    public void setUsuarioActual(Usuario usuario) {
+        this.usuarioActual = usuario;
+    }
 
     @FXML
-    private TextField filtroApellidos;
+    private void regresar(ActionEvent event) throws IOException {
 
-    @FXML
-    private ComboBox<String> comboDepartamentos;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/menu/OpcionesDeMenuPoliciaView.fxml"));
+        Parent root = loader.load();
 
-    @FXML
-    private TableView<DelincuentePoliciaView> tablaDelincuentes;
+        OpcionesDelMenuPoliciaController controller = loader.getController();
+        controller.iniciarDatos(this.usuarioActual);
 
-    @FXML
-    private TableColumn<DelincuentePoliciaView, String> columnaNombre;
-
-    @FXML
-    private TableColumn<DelincuentePoliciaView, String> columnaApellidos;
-
-    @FXML
-    private TableColumn<DelincuentePoliciaView, String> columnnaDelito;
-
-    @FXML
-    private TableColumn<DelincuentePoliciaView, Double> columnaRecompensa;
-
-    @FXML
-    private ObservableList<DelincuentePoliciaView> listaObservableDelincuente;
-
-
-
+        Scene escena = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Menú Policía");
+        stage.setScene(escena);
+        stage.show();
+    }
 }
