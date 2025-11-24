c7e10fe7f94929a8c263b4bf47194d666c194630
Fabricio <fabricioayaranga@gmail.com>
2025-11-24 16:51:37 -0500

    actualizando clase para ver reportes como policia

---
+++ secure-access-frontend/src/main/java/org/secureaccess/app/secureaccessfrontend/viewModels/ReportePoliciaView.java
@@ -0,0 +1,39 @@
+package org.secureaccess.app.secureaccessfrontend.viewModels;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ReportePoliciaView {
+
+    private final StringProperty enviadoPor;
+    private final StringProperty supuestoDelito;
+    private final StringProperty fechaReporte;
+    private final StringProperty nivelUrgencia;
+
+    public ReportePoliciaView(String enviadoPor,
+                              String supuestoDelito,
+                              String fechaReporte,
+                              String nivelUrgencia) {
+        this.enviadoPor = new SimpleStringProperty(enviadoPor);
+        this.supuestoDelito = new SimpleStringProperty(supuestoDelito);
+        this.fechaReporte = new SimpleStringProperty(fechaReporte);
+        this.nivelUrgencia = new SimpleStringProperty(nivelUrgencia);
+    }
+
+    public StringProperty enviadoPorProperty() {
+        return enviadoPor;
+    }
+
+    public StringProperty supuestoDelitoProperty() {
+        return supuestoDelito;
+    }
+
+    public StringProperty fechaReporteProperty() {
+        return fechaReporte;
+    }
+
+    public StringProperty nivelUrgenciaProperty() {
+        return nivelUrgencia;
+    }
+
+}
